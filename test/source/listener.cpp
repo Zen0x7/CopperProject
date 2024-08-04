6989776cfdf1e16ac31251e5ea61af23152195a5
Ian Torres <iantorres@outlook.com>
2024-08-03 21:16:19 -0400

    Style fixed.

--- test/source/listener.cpp
+++ test/source/listener.cpp
@@ -5,129 +5,87 @@
 
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/address.hpp>
-#include <boost/smart_ptr/make_shared_object.hpp>
-#include <boost/thread.hpp>
-#include <thread>
-
-
+#include <boost/asio/strand.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
-#include <boost/asio/strand.hpp>
+#include <boost/smart_ptr/make_shared_object.hpp>
+#include <boost/thread.hpp>
 #include <cstdlib>
 #include <functional>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
 
-void
-fail(boost::beast::error_code ec, char const* what)
-{
-    std::cerr << what << ": " << ec.message() << "\n";
+void fail(boost::beast::error_code ec, char const* what) {
+  std::cerr << what << ": " << ec.message() << "\n";
 }
 
-class http_client : public std::enable_shared_from_this<http_client>
-{
-    boost::asio::ip::tcp::resolver resolver_;
-    boost::beast::tcp_stream stream_;
-    boost::beast::flat_buffer buffer_; // (Must persist between reads)
-    boost::beast::http::request<boost::beast::http::empty_body> req_;
-    boost::beast::http::response<boost::beast::http::string_body> res_;
+class http_client : public std::enable_shared_from_this<http_client> {
+  boost::asio::ip::tcp::resolver resolver_;
+  boost::beast::tcp_stream stream_;
+  boost::beast::flat_buffer buffer_;  // (Must persist between reads)
+  boost::beast::http::request<boost::beast::http::empty_body> req_;
+  boost::beast::http::response<boost::beast::http::string_body> res_;
 
 public:
-    explicit
-    http_client(boost::asio::io_context& ioc)
-        : resolver_(boost::asio::make_strand(ioc))
-        , stream_(boost::asio::make_strand(ioc))
-    {
-    }
+  explicit http_client(boost::asio::io_context& ioc)
+      : resolver_(boost::asio::make_strand(ioc)), stream_(boost::asio::make_strand(ioc)) {}
 
-    void
-    run(
-        char const* host,
-        char const* port,
-        char const* target,
-        int version)
-    {
-        req_.version(version);
-        req_.method(boost::beast::http::verb::get);
-        req_.target(target);
-        req_.set(boost::beast::http::field::host, host);
-        req_.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+  void run(char const* host, char const* port, char const* target, int version) {
+    req_.version(version);
+    req_.method(boost::beast::http::verb::get);
+    req_.target(target);
+    req_.set(boost::beast::http::field::host, host);
+    req_.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-        resolver_.async_resolve(
-            host,
-            port,
-            boost::beast::bind_front_handler(
-                &http_client::on_resolve,
-                shared_from_this()));
-    }
+    resolver_.async_resolve(
+        host, port, boost::beast::bind_front_handler(&http_client::on_resolve, shared_from_this()));
+  }
 
-    void
-    on_resolve(
-        boost::beast::error_code ec,
-        boost::asio::ip::tcp::resolver::results_type results)
-    {
-        if(ec)
-            return fail(ec, "resolve");
+  void on_resolve(boost::beast::error_code ec,
+                  boost::asio::ip::tcp::resolver::results_type results) {
+    if (ec) return fail(ec, "resolve");
 
-        stream_.expires_after(std::chrono::seconds(30));
+    stream_.expires_after(std::chrono::seconds(30));
 
-        stream_.async_connect(
-            results,
-            boost::beast::bind_front_handler(
-                &http_client::on_connect,
-                shared_from_this()));
-    }
+    stream_.async_connect(
+        results, boost::beast::bind_front_handler(&http_client::on_connect, shared_from_this()));
+  }
 
-    void
-    on_connect(boost::beast::error_code ec, boost::asio::ip::tcp::resolver::results_type::endpoint_type)
-    {
-        if(ec)
-            return fail(ec, "connect");
+  void on_connect(boost::beast::error_code ec,
+                  boost::asio::ip::tcp::resolver::results_type::endpoint_type) {
+    if (ec) return fail(ec, "connect");
 
-        stream_.expires_after(std::chrono::seconds(30));
+    stream_.expires_after(std::chrono::seconds(30));
 
-        boost::beast::http::async_write(stream_, req_,
-            boost::beast::bind_front_handler(
-                &http_client::on_write,
-                shared_from_this()));
-    }
+    boost::beast::http::async_write(
+        stream_, req_,
+        boost::beast::bind_front_handler(&http_client::on_write, shared_from_this()));
+  }
 
-    void
-    on_write(
-        boost::beast::error_code ec,
-        std::size_t bytes_transferred)
-    {
-        boost::ignore_unused(bytes_transferred);
+  void on_write(boost::beast::error_code ec, std::size_t bytes_transferred) {
+    boost::ignore_unused(bytes_transferred);
 
-        if(ec)
-            return fail(ec, "write");
+    if (ec) return fail(ec, "write");
 
-        boost::beast::http::async_read(stream_, buffer_, res_,
-            boost::beast::bind_front_handler(
-                &http_client::on_read,
-                shared_from_this()));
-    }
+    boost::beast::http::async_read(
+        stream_, buffer_, res_,
+        boost::beast::bind_front_handler(&http_client::on_read, shared_from_this()));
+  }
 
-    void
-    on_read(
-        boost::beast::error_code ec,
-        std::size_t bytes_transferred)
-    {
-        boost::ignore_unused(bytes_transferred);
+  void on_read(boost::beast::error_code ec, std::size_t bytes_transferred) {
+    boost::ignore_unused(bytes_transferred);
 
-        if(ec)
-            return fail(ec, "read");
+    if (ec) return fail(ec, "read");
 
-        std::cout << res_ << std::endl;
+    std::cout << res_ << std::endl;
 
-        stream_.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+    stream_.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
 
-        if(ec && ec != boost::beast::errc::not_connected)
-            return fail(ec, "shutdown");
-
-    }
+    if (ec && ec != boost::beast::errc::not_connected) return fail(ec, "shutdown");
+  }
 };
 
 TEST_CASE("Serve") {
