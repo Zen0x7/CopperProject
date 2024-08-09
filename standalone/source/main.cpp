5ffc2b302b5dd09b53bc737fa6431422b88bb057
Ian Torres <iantorres@outlook.com>
2024-08-08 23:16:32 -0400

    Implementing redis.

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -5,14 +5,82 @@
 #include <copper/state_container.h>
 #include <copper/version.h>
 
+#include <coroutine>
+
+
+#include <boost/redis/connection.hpp>
+#include <boost/redis/logger.hpp>
+#include <boost/asio/awaitable.hpp>
+#include <boost/asio/use_awaitable.hpp>
+#include <boost/asio/deferred.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/use_awaitable.hpp>
 #include <boost/asio/io_context.hpp>
-#include <boost/asio/ip/address.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/consign.hpp>
+#include <boost/asio/redirect_error.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <boost/redis/src.hpp>
 #include <boost/smart_ptr/make_shared_object.hpp>
 #include <boost/stacktrace.hpp>
+#include <boost/thread.hpp>
 #include <cxxopts.hpp>
 #include <iostream>
 #include <string>
 #include <termcolor/termcolor.hpp>
+
+using signal_set = boost::asio::deferred_t::as_default_on_t<boost::asio::signal_set>;
+
+auto deep_receiver(boost::shared_ptr<boost::redis::connection> conn) -> boost::asio::awaitable<void> {
+  boost::redis::request req;
+  req.push("SUBSCRIBE", "channel");
+
+  boost::redis::generic_response resp;
+
+  conn->set_receive_response(resp);
+
+  while (conn->will_reconnect()) {
+
+    co_await conn->async_exec(req, boost::redis::ignore, boost::asio::deferred);
+
+    for (boost::system::error_code ec;;) {
+      conn->receive(ec);
+
+      if (ec == boost::redis::error::sync_receive_push_failed) {
+        ec = {};
+
+        co_await conn->async_receive(boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+      }
+
+      if (ec)
+        break;
+
+
+      std::cout
+         << resp.value().at(1).value
+         << " " << resp.value().at(2).value
+         << " " << resp.value().at(3).value
+         << std::endl;
+
+      boost::redis::consume_one(resp);
+    }
+  }
+}
+
+auto co_receiver(boost::redis::config cfg) -> boost::asio::awaitable<void> {
+  auto ex = co_await boost::asio::this_coro::executor;
+  auto conn = boost::make_shared<boost::redis::connection>(ex);
+
+  boost::asio::co_spawn(ex, deep_receiver(conn), boost::asio::detached);
+
+  conn->async_run(cfg, {}, boost::asio::consign(boost::asio::detached, conn));
+
+  signal_set sig_set(ex, SIGINT, SIGTERM);
+  co_await sig_set.async_wait();
+  conn->cancel();
+}
+
 
 auto main(int argc, char** argv) -> int {
   using namespace copper;
@@ -49,8 +117,25 @@ auto main(int argc, char** argv) -> int {
   auto threads_number_ = std::max(1, service_threads_);
 
   boost::asio::io_context io_context_;
+  boost::asio::io_context redis_io_context_;
 
   auto state_ = boost::make_shared<state>();
+
+  boost::redis::config cfg;
+
+  cfg.addr.host = "127.0.0.1";
+  cfg.addr.port = "6379";
+
+  boost::asio::co_spawn(redis_io_context_, co_receiver(cfg), [] (std::exception_ptr p) {
+    if (p)
+      std::rethrow_exception(p);
+  });
+
+  boost::thread thread([&redis_io_context_]() {
+    redis_io_context_.run();
+  });
+
+  thread.detach();
 
   boost::make_shared<listener>(
       io_context_, boost::asio::ip::tcp::endpoint{service_address_, service_port_}, state_)
